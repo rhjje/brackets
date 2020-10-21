2c215171944ee70546a4d79b81dcdfb769e2588d
rhjje <numelen@gmail.com>
2020-10-21 17:39:44 +0300

    feat: done brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let brackets = [];
+    let result = str;
+
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        brackets[i] = bracketsConfig[i].join('');
+    }
+    let count = brackets.length - 1;
+    while (count >= 0) {
+        if (result.includes(brackets[count])) {
+            result = result.replace(brackets[count], '');
+            count = brackets.length;
+        } else {
+            count--;
+        }
+    }
+
+    return result === '';
 }
